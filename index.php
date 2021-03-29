109c65c72eacf667aff944497cac32769bc42b26
frostkom <martin.fors@frostkom.se>
2021-03-29 09:26:38 +0200

    Make sure that Base is active

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Backup
 Plugin URI: https://github.com/frostkom/mf_backup
 Description: 
-Version: 2.2.2
+Version: 2.2.3
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://frostkom.se
@@ -14,36 +14,39 @@ Depends: MF Base
 GitHub Plugin URI: frostkom/mf_backup
 */
 
-include_once("include/classes.php");
-
-$obj_backup = new mf_backup();
-
-add_action('cron_base', array($obj_backup, 'cron_base'), mt_rand(1, 10));
-
-if(is_admin())
+if(is_plugin_active("mf_base/index.php"))
 {
-	register_activation_hook(__FILE__, 'activate_backup');
-	register_uninstall_hook(__FILE__, 'uninstall_backup');
+	include_once("include/classes.php");
 
-	add_action('admin_init', array($obj_backup, 'settings_backup'));
-	add_action('admin_init', array($obj_backup, 'admin_init'), 0);
+	$obj_backup = new mf_backup();
 
-	load_plugin_textdomain('lang_backup', false, dirname(plugin_basename(__FILE__))."/lang/");
-}
+	add_action('cron_base', array($obj_backup, 'cron_base'), mt_rand(1, 10));
 
-add_action('wp_ajax_perform_backup', array($obj_backup, 'perform_backup'));
+	if(is_admin())
+	{
+		register_activation_hook(__FILE__, 'activate_backup');
+		register_uninstall_hook(__FILE__, 'uninstall_backup');
 
-function activate_backup()
-{
-	mf_uninstall_plugin(array(
-		'options' => array('setting_backup_mysql', 'setting_backup_perform'),
-	));
-}
+		add_action('admin_init', array($obj_backup, 'settings_backup'));
+		add_action('admin_init', array($obj_backup, 'admin_init'), 0);
 
-function uninstall_backup()
-{
-	mf_uninstall_plugin(array(
-		'uploads' => 'mf_backup',
-		'options' => array('setting_backup_schedule', 'setting_backup_sites', 'setting_backup_perform', 'setting_rss_api_key', 'setting_rss_url', 'setting_backup_limit', 'setting_backup_compress', 'setting_backup_db_type', 'setting_backup_db_tables', 'setting_backup_db_tables'),
-	));
+		load_plugin_textdomain('lang_backup', false, dirname(plugin_basename(__FILE__))."/lang/");
+	}
+
+	add_action('wp_ajax_perform_backup', array($obj_backup, 'perform_backup'));
+
+	function activate_backup()
+	{
+		mf_uninstall_plugin(array(
+			'options' => array('setting_backup_mysql', 'setting_backup_perform'),
+		));
+	}
+
+	function uninstall_backup()
+	{
+		mf_uninstall_plugin(array(
+			'uploads' => 'mf_backup',
+			'options' => array('setting_backup_schedule', 'setting_backup_sites', 'setting_backup_perform', 'setting_rss_api_key', 'setting_rss_url', 'setting_backup_limit', 'setting_backup_compress', 'setting_backup_db_type', 'setting_backup_db_tables', 'setting_backup_db_tables'),
+		));
+	}
 }
